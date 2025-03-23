03274fa220f32d86fe6a81a5144f063e773afdc8
eglab <eryko2002@gmail.com>
2025-03-23 16:03:12 +0100

    update

--- dap-r1/dap-ea.py
+++ dap-r1/dap-ea.py
@@ -3,6 +3,8 @@ import json
 from read_config_net4 import *
 
 num_chromosomes = 10
+num_generations=5
+
 # Funkcja do obliczania wartości celu
 def calculate_objective_value(demandPath_flow, demand_max_path, demand_volume, demand_path_links, link_capacity):
     # Inicjalizacja zmiennych
@@ -23,22 +25,44 @@ def calculate_objective_value(demandPath_flow, demand_max_path, demand_volume, d
     
     return max_link_overload, demandPath_flow
 
+def select_parents(population, fitness_values):
+    # Odwracamy wartości funkcji celu, aby mniejsze wartości miały większe prawdopodobieństwo
+    inverted_fitness = 1 / np.array(fitness_values)
+    selection_probabilities = inverted_fitness / np.sum(inverted_fitness)
+    sum_of_probabilities=np.sum(selection_probabilities)
+    
 
-def roulette_wheel_selection(population, probabilities):
-    # Normalizowanie prawdopodobieństw (suma wszystkich prawdopodobieństw powinna wynosić 1)
-    total_probability = sum(probabilities)
-    normalized_probabilities = [prob / total_probability for prob in probabilities]
+    #print("Fitness_values:{}\n".format(fitness_values))
+    #print("Inverted_fitness:{}\n".format(inverted_fitness))
+    #print("Selection_probabilities:{}\n".format(selection_probabilities))
+    #print("Sum of probabilities:{}\n".format(sum_of_probabilities))
+
+    # Wybór dwóch rodziców na podstawie rozkładu prawdopodobieństwa
+    parents_indices = np.random.choice(len(population), size=2, p=selection_probabilities)
     
-    # Akumulowanie prawdopodobieństw
-    cumulative_probabilities = np.cumsum(normalized_probabilities)
+    parent1 = population[parents_indices[0]].T
+    parent2 = population[parents_indices[1]].T
     
-     # Losowanie wartości z przedziału [0, 1]
-    random_value = np.random.random()  # Losujemy liczbę zmiennoprzecinkową z przedziału [0, 1]
+    return parent1, parent2
+
+
+# Funkcja główna (algorytm genetyczny)
+def roulette_wheel_selection_algorithm():
+    population = list(chromosomes_data.values())  # Wczytanie populacji z chromosomes_data
     
-    # Znajdujemy chromosom, który pasuje do wylosowanej wartości
-    for i, cumulative_probability in enumerate(cumulative_probabilities):
-        if random_value <= cumulative_probability:
-            return i, population[i]  # Zwracamy indeks (numer) i chromosom
+    for generation in range(num_generations):
+        fitness_values = []
+        
+        # Obliczanie wartości fitness dla każdej macierzy (chromosomu)
+        for chromosome in population:
+            z_value, _ = calculate_objective_value(chromosome, demand_max_path, demand_volume, demand_path_links, link_capacity)
+            fitness_values.append(z_value)
+        
+        parent1, parent2 = select_parents(population, fitness_values)
+        print("--------------------------------------------------------------------------------")
+        print(f"Generation {generation + 1}: Selected parents")
+        print(f"Parent 1:\n{parent1}")
+        print(f"Parent 2:\n{parent2}")
 
 def main_model():
     #obliczamy wartość funkcji celu dla wzorca:
@@ -54,9 +78,10 @@ def main_candidate(candidate_number):
     print(f'Flow table for Chromosom {candidate_number}:\n{demandPath_flow.T}')
 
 if __name__=="__main__":
-    main_model()
-    print()
-    for candidate_number in range(num_chromosomes):
-        print('========================================================')
-        main_candidate(candidate_number=candidate_number+1)
+    #main_model()
+    #print()
+    #for candidate_number in range(num_chromosomes):
+    #    print('========================================================')
+    #    main_candidate(candidate_number=candidate_number+1)
+    roulette_wheel_selection_algorithm()
 
